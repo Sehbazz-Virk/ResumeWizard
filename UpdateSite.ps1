88b5c35f7b952ba419f92cf6ebde445bb92c814d
virkSehbazz12 <71534099+virkSehbazz12@users.noreply.github.com>
2022-09-16 12:39:14 -0600

    Updating site with new changes on September 16, 2022 12:39:13 PM

---
+++ UpdateSite.ps1
@@ -0,0 +1,11 @@
+﻿# Build single file verion of Wizard and move to location expected by Github Pages
+tiddlywiki SMAwizard --build index
+Move-Item -Path .\SMAwizard\output\index.html -Destination .\docs\index.html
+
+#Get date and time for git commit message
+$DateTime = (Get-Date).DateTime
+$commitMessage = "Updating site with new changes on " + $DateTime
+
+git add --all
+git commit -m $commitMessage
+git push
