05666c356d723162656d77337275d4aefbd904d2
virkSehbazz12 <71534099+virkSehbazz12@users.noreply.github.com>
2022-09-16 13:00:06 -0600

    Finished UpdateSite powershell script

--- UpdateSite.ps1
+++ UpdateSite.ps1
@@ -1,17 +1,14 @@
 ﻿#Move old index file to backups
 $backupDate = Get-Date -Format "dd/MM/yyyy_HHmm"
 $Destination = ".\backup\index_" + $backupDate + ".html"
-Write-Output $Destination
 Move-Item -Path .\docs\index.html -Destination $Destination
 
-# Build single file verion of Wizard and move to location expected by Github Pages
+# Build single html file verion of Wizard and move to location expected by Github Pages
 tiddlywiki SMAwizard --build index
 Copy-Item -Path .\SMAwizard\output\index.html -Destination .\docs\index.html
 
-# Get commit message
-$commitMessage = Read-Host -Prompt "Commit Message"
-
 # commit and push changes
+$commitMessage = Read-Host -Prompt "Commit Message"
 git add --all
 git commit -m $commitMessage
 git push
