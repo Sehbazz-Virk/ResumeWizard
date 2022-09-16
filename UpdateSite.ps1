6ca01fb337b4080ebf3397d09ec06ae9b72d526d
virkSehbazz12 <71534099+virkSehbazz12@users.noreply.github.com>
2022-09-16 12:44:35 -0600

    Testing UpdateSite script

--- UpdateSite.ps1
+++ UpdateSite.ps1
@@ -4,7 +4,7 @@ Remove-Item -Path .\docs\index.html
 Move-Item -Path .\SMAwizard\output\index.html -Destination .\docs\index.html
 
 # Get commit message
-$commitMessage = Read-Host -Prompt "Commit Message: "
+$commitMessage = Read-Host -Prompt "Commit Message"
 
 # commit and push changes
 git add --all
